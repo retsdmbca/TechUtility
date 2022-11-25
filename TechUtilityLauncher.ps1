9321f9af3f9afc2dcf4c252a1e09e03b94aed85a
Jason Bergen <jbergen@retsd.mb.ca>
2022-11-25 15:55:36 -0600

    added launcher ps1

---
+++ TechUtilityLauncher.ps1
@@ -0,0 +1,5 @@
+﻿if (!(test-path "C:\ProgramData\RETSD")){New-Item -Path "C:\ProgramData\RETSD" -ItemType directory}
+if (!(test-path "C:\ProgramData\RETSD\CMTrace.exe")){Invoke-WebRequest -Uri https://github.com/retsdmbca/TechUtility/blob/master/CMTrace.exe?raw=true -OutFile C:\ProgramData\RETSD\CMTrace.exe}
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/retsdmbca/TechUtility/master/TechUtility.ps1 -OutFile C:\ProgramData\RETSD\TechUtility.ps1
+
+powershell C:\ProgramData\RETSD\TechUtility.ps1
