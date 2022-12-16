3c93e5f1c063ff66cf872830dd2232adc8ee8fa6
Jason Bergen <jbergen@retsd.mb.ca>
2022-12-16 15:35:22 -0600

    3:35

--- Install.ps1
+++ Install.ps1
@@ -6,4 +6,8 @@ if (!(test-path "C:\ProgramData\RETSD\CMTrace.exe")) {copy-item 'CMTrace.exe' 'C
 copy-item 'TechUtilityLauncher.exe' "C:\ProgramData\RETSD\Tech Utility App\TechUtilityLauncher.exe" -Force
 
 #Install-Module ps2exe
-#Invoke-ps2exe -inputfile "D:\Git Repository\TechUtility\TechUtilityLauncher.ps1" -outputfile "D:\Git Repository\TechUtility\TechUtilityLauncher.exe" -iconFile "D:\ico files\RETSDLogo.ico"
+#Invoke-ps2exe -inputfile "D:\Git Repository\TechUtility\TechUtilityLauncher.ps1" -outputfile "D:\Git Repository\TechUtility\TechUtilityLauncher.exe" -iconFile "D:\ico files\RETSDLogo.ico" -noConsole
+
+Function UpdateSCCM {
+    copy-item 'install.ps1' "\\ao-sccm\source\scripts\Tech Utility App\install.ps1" -Force
+    copy-item 'TechUtilityLauncher.exe' "\\ao-sccm\source\scripts\Tech Utility App\TechUtilityLauncher.exe" -Force}
