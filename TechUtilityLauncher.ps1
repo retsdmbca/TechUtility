7df955429a54c7dce973aad838f05d553aa61696
Jason Bergen <jbergen@retsd.mb.ca>
2022-12-16 11:26:10 -0600

    .

--- TechUtilityLauncher.ps1
+++ TechUtilityLauncher.ps1
@@ -1,10 +1,12 @@
-﻿if (!(test-path "C:\ProgramData\RETSD")){New-Item -Path "C:\ProgramData\RETSD" -ItemType directory}
+﻿cls
+if (!(test-path "C:\ProgramData\RETSD")){New-Item -Path "C:\ProgramData\RETSD" -ItemType directory}
 if (!(test-path "C:\ProgramData\RETSD\CMTrace.exe")){Invoke-WebRequest -Uri https://github.com/retsdmbca/TechUtility/blob/master/CMTrace.exe?raw=true -OutFile C:\ProgramData\RETSD\CMTrace.exe}
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/retsdmbca/TechUtility/master/TechUtility.ps1 -OutFile C:\ProgramData\RETSD\TechUtility.ps1
 
 Function Run {
-    if ($RadioButton1.Checked -eq $true) {Start-Process powershell.exe -ArgumentList '-noprofile -file C:\ProgramData\RETSD\TechUtility.ps1' }
-    if ($RadioButton2.Checked -eq $true) {Start-Process powershell.exe -ArgumentList '-noprofile -file C:\ProgramData\RETSD\TechUtility.ps1' -Verb RunAs}
+    $PID
+    if ($RadioButton1.Checked -eq $true) {Start-Process powershell.exe -ArgumentList '-WindowStyle Hidden -noprofile -file C:\ProgramData\RETSD\TechUtility.ps1' }
+    if ($RadioButton2.Checked -eq $true) {Start-Process powershell.exe -ArgumentList '-WindowStyle Hidden -noprofile -file C:\ProgramData\RETSD\TechUtility.ps1' -Verb RunAs}
 }
 
 
