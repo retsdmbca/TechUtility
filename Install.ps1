fef998a1e8ecc3d525bc99a5b6acf95b51335ee0
Jason Bergen <jbergen@retsd.mb.ca>
2022-12-20 09:31:54 -0600

    .

--- Install.ps1
+++ Install.ps1
@@ -1,7 +1,7 @@
 if (!(test-path "C:\ProgramData\RETSD")){New-Item -Path "C:\ProgramData\RETSD" -ItemType directory}
 if (!(test-path "C:\ProgramData\RETSD\Tech Utility App")) {New-Item -Path "C:\ProgramData\RETSD\Tech Utility App" -ItemType directory}
 if (!(test-path "C:\ProgramData\RETSD\Tech Utility App\configs")) {New-Item -Path "C:\ProgramData\RETSD\Tech Utility App\configs" -ItemType directory}
-if (!(test-path "C:\ProgramData\RETSD\Tech Utility App\Logs")) {New-Item -Path "C:\ProgramData\RETSD\Logs" -ItemType directory}
+if (!(test-path "C:\ProgramData\RETSD\Tech Utility App\Logs")) {New-Item -Path "C:\ProgramData\RETSD\Tech Utility App\Logs" -ItemType directory}
 if (!(test-path "C:\ProgramData\RETSD\CMTrace.exe")) {copy-item 'CMTrace.exe' 'C:\ProgramData\RETSD\CMTrace.exe'}
 if (!(test-path "C:\ProgramData\RETSD\Tech Utility App\configs\RETSDLogo.ico")) {copy-item 'RETSDLogo.ico' 'C:\ProgramData\RETSD\Tech Utility App\configs\RETSDLogo.ico'}
 
--- TechUtilityLauncher.ps1
+++ TechUtilityLauncher.ps1
@@ -1,13 +1,28 @@
 ﻿cls
 if (!(test-path "C:\ProgramData\RETSD")){New-Item -Path "C:\ProgramData\RETSD" -ItemType directory}
 if (!(test-path "C:\ProgramData\RETSD\Tech Utility App")) {New-Item -Path "C:\ProgramData\RETSD\Tech Utility App" -ItemType directory}
-if (!(test-path "C:\ProgramData\RETSD\Tech Utility App\configs")) {New-Item -Path "C:\ProgramData\RETSD\Tech Utility App\configs" -ItemType directory}
+if (!(test-path "C:\ProgramData\RETSD\Tech Utility App\configs")) {New-Item -Path "C:\ProgramData\RETSD\Tech Utility App\Configs" -ItemType directory}
+if (!(test-path "C:\ProgramData\RETSD\Tech Utility App\Logs")) {New-Item -Path "C:\ProgramData\RETSD\Tech Utility App\Logs" -ItemType directory}
 if (!(test-path "C:\ProgramData\RETSD\CMTrace.exe")){Invoke-WebRequest -Uri https://github.com/retsdmbca/TechUtility/blob/master/CMTrace.exe?raw=true -OutFile C:\ProgramData\RETSD\CMTrace.exe}
 if (!(test-path "C:\ProgramData\RETSD\configs\RETSDLogo.ico")){Invoke-WebRequest -Uri https://github.com/retsdmbca/TechUtility/blob/master/RETSDLogo.ico?raw=true -OutFile "C:\ProgramData\RETSD\Tech Utility App\configs\RETSDLogo.ico"}
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/retsdmbca/TechUtility/master/TechUtility.ps1 -OutFile "C:\ProgramData\RETSD\Tech Utility App\TechUtility.ps1"
-    
+
+Function GithubTest {
+    $HTTP_Request = [System.Net.WebRequest]::Create('http://github.com')
+    $HTTP_Response = $HTTP_Request.GetResponse()
+    $HTTP_Status = [int]$HTTP_Response.StatusCode
+    If ($HTTP_Status -eq 200) {Run}
+    Else {
+        $Labeloutput.Visible = $true
+        $Labeloutput2.Visible = $true
+        $Labeloutput.Text = "Can't connect to Github."
+    }
+}
 Function Run {
+    $HTTP_Response.Close()
+    $Labeloutput.Text = "Please Wait..."
     $Labeloutput.Visible = $true
+    $ButtonRun.Visible = $false
     if ($RadioButton1.Checked -eq $true) {
         write-output "normal" | out-file -filepath "C:\ProgramData\RETSD\Tech Utility App\configs\state.txt"
         Start-Process powershell.exe -ArgumentList '-WindowStyle Hidden -noprofile -file "C:\ProgramData\RETSD\Tech Utility App\TechUtility.ps1"' }
@@ -21,7 +36,7 @@ Add-Type -assembly System.Windows.Forms
 $main_form = New-Object System.Windows.Forms.Form
 $main_form.Text ='Tech Utility'
 $main_form.Width = 210
-$main_form.Height = 220
+$main_form.Height = 230
 $main_form.StartPosition = 'CenterScreen'
 $main_form.FormBorderStyle = 'Fixed3D'
 $main_form.AutoSize = $false
@@ -53,7 +68,7 @@ $ButtonRun = New-Object System.Windows.Forms.Button
 $ButtonRun.Location = New-Object System.Drawing.Size(20,110)
 $ButtonRun.Size = New-Object System.Drawing.Size(150,23)
 $ButtonRun.Text = "Run Program"
-$ButtonRun.Add_Click({Run})
+$ButtonRun.Add_Click({GithubTest})
 $main_form.Controls.Add($ButtonRun)
 
 $Labeloutput = New-Object System.Windows.Forms.Label
@@ -62,5 +77,12 @@ $Labeloutput.Location  = New-Object System.Drawing.Point(20,140)
 $Labeloutput.AutoSize = $true
 $Labeloutput.Visible = $false
 $main_form.Controls.Add($Labeloutput)
+
+$Labeloutput2 = New-Object System.Windows.Forms.Label
+$Labeloutput2.Text = "Check Connection"
+$Labeloutput2.Location  = New-Object System.Drawing.Point(20,160)
+$Labeloutput2.AutoSize = $true
+$Labeloutput2.Visible = $false
+$main_form.Controls.Add($Labeloutput2)
 
 $main_form.ShowDialog()
